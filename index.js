d8ccf73df214bbe680add29593d26aaed82c673a
Ana Ea <aabad0464@gmail.com>
2021-06-23 09:42:33 -0600

    new repo

---
+++ index.js
@@ -0,0 +1,6 @@
+const Employee  = require("./test/Employee.test");
+const  Engineer  = require("./test/Engineer.test");
+const  Intern  = require("./test/Intern.test");
+const Manager  = require("./test/Manager.test");
+const inquirer = require("inquirer");
+const fs = require("fs");
