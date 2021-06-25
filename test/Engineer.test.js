64bf0f1e273094b62844df678eccbeef481b84cb
Ana Ea <aabad0464@gmail.com>
2021-06-25 17:10:06 -0600

    added fucntion gitHub

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,1 +1,22 @@
+const Engineer = require("../lib/Engineer");
 
+
+test ("Checking for Github user", () => {
+    const gitHub = "abanae";
+    const employee = new Engineer("Eric", 12, "aabd@gmail.com", gitHub)
+    expect(employee.gitHub).toBe(gitHub)
+})
+
+
+
+
+
+
+
+
+
+
+
+// github—GitHub username
+// getGithub()
+// getRole()—overridden to return 'Engineer'
