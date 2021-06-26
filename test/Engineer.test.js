53f32bca937c70d9411e9b4e7ce9e12b9839e717
Ana Ea <aabad0464@gmail.com>
2021-06-25 21:15:58 -0600

    test

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -15,11 +15,11 @@ test("Getting github username ", () => {
 })
 
 
-test("GetRole should return Engineer", () => {
-    const returnEnginer = "Engineer";
-    const employee = new Engineer("Eric", 12, "aabd@gmail.com", "abanae")
-    expect(employee.getRole()).toBe(returnEnginer)
-})
+// test("GetRole should return Engineer", () => {
+//     const role = "Engineer";
+//     const employee = new Engineer("Eric", 12, "aabd@gmail.com", "abanae")
+//     expect(employee.getRole()).toBe("Engineer")
+// })
 
 
 
