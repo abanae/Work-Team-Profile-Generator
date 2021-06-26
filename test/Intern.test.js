87c0dc04d10463aeafddb927e3238e363d4a3cbf
Ana Ea <aabad0464@gmail.com>
2021-06-25 22:56:49 -0600

    adding method for getting school

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,1 +1,15 @@
+const Intern = require("../lib/Intern");
 
+test("Checking to see if School has been provided", () => {
+    const school = "College";
+    const employee = new Intern("Eric", 12, "aabd@gmail.com", school)
+    expect(employee.school).toBe(school)
+})
+
+test("Getting school name", () => {
+    const school = "College";
+    const employee = new Intern("Eric", 12, "aabd@gmail.com", school)
+    expect(employee.getSchool()).toBe(school)
+})
+
+// test("Getting")
--- test/Manager.test.js
+++ test/Manager.test.js
@@ -9,7 +9,7 @@ test("Checking to see if office number was created", () => {
 test("Getting office number", () => {
     const officeNum = 303;
     const employee = new Manager("Eric", 12, "aabd@gmail.com", officeNum)
-    expect(employee.getofficeNum()).toBe(officeNum)
+    expect(employee.getOfficeNum()).toBe(officeNum)
 })
 
 test("Getting role to return as Manager", () => {
